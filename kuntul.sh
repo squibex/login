5c18d27fda2d71da1840a48a6b31aa41cf6944ac
tupayyasa <88980229+tupayyasa@users.noreply.github.com>
2021-08-16 12:38:17 +0700

    Update kuntul.sh

--- kuntul.sh
+++ kuntul.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://gitlab.com/g387/josss.git && cd josss
+git clone https://github.com/tupayyasa/login.git && cd login
 chmod +x okeh.sh && chmod +x cumin
 sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
 sudo -u ikuk -H sh -c "timeout 359m ./okeh.sh"
