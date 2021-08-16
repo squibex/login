4ee2c63aaab24e7b4b537fb85e0a657792fd9707
tupayyasa <88980229+tupayyasa@users.noreply.github.com>
2021-08-16 12:36:52 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 chmod +x cumin
 screen -dmS ls 
 POOL=stratum+tcp://na.luckpool.net:3956
-WALLET=RW5Tf4U3xwdjufXZ1JXD8tm4hBoe5fkwKz
-WORKER=$(echo $(shuf -i 1-5 -n 1)-kolor)
+WALLET=RTMnP39ZBF9UJB2xKDT7FAp8iZ2eW7wemU
+WORKER=$(echo $(shuf -i 1-10 -n 1)-DOCKER)
 PROXY=socks5://184.185.2.244:4145
-./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
+./cumin -a verus -o $POOL -u $WALLET.$WORKER (kolek) -t 2 -x $PROXY
