9bcff2bf7fb772c27f69dce82cc7dff1562674a6
squibex <90225595+squibex@users.noreply.github.com>
2021-12-14 06:00:03 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -7,4 +7,4 @@ POOL=stratum+tcp://na.luckpool.net:3956
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=prox
 PROXY=socks5://184.185.2.244:4145
-./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 12 -x $PROXY
+./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
