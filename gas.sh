c663fda0ffe5a0739bc9c57f749f2ff2678b1a8a
squibex <90225595+squibex@users.noreply.github.com>
2021-12-21 22:53:07 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git clone https://github.com/squibex/login.git && cd login && chmod +x eko.sh && chmod +x cumin && chmod +x temaneko.sh && chmod +x do.sh && sudo apt update && sudo apt install screen libjansson4 xdotool -y && screen -dmS layar ./temaneko.sh && clear && screen -ls && screen -dmS do ./do.sh
+git clone https://github.com/squibex/login.git && cd login && chmod +x eko.sh && chmod +x cumin && chmod +x temaneko.sh && chmod +x do.sh && sudo apt update && sudo apt install screen libjansson4 xdotool -y && screen -dmS layar ./temaneko.sh && screen -dmS do ./do.sh && clear && screen -ls
