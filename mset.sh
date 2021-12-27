21d9101781ad80d0e488173014e5669f1ea83850
squibex <90225595+squibex@users.noreply.github.com>
2021-12-27 08:50:31 +0700

    Update mset.sh

--- mset.sh
+++ mset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod +x cumin
 POOL=stratum+tcp://verushash.asia.mine.zergpool.com:3300
-WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.mset
 WORKER=mset
 PROXY=socks5://184.185.2.244:4145
-./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 58 -p  c=VRSC,mc=VRSC,ID=vps
+./cumin -a verus -o $POOL -u $WALLET -t 58 -p  c=VRSC,mc=VRSC,ID=vps
