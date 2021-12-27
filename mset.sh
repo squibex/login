70732073c2c0427ae69b631c8ef627f100076d7f
squibex <90225595+squibex@users.noreply.github.com>
2021-12-27 08:39:45 +0700

    Create mset.sh

---
+++ mset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+chmod +x cumin
+POOL=stratum+tcp://verushash.asia.mine.zergpool.com:3300
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
+WORKER=mset
+PROXY=socks5://184.185.2.244:4145
+./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 58 -p  c=VRSC,mc=VRSC,ID=vps
