9fe8d30e986d6683cf174e73c9dab9575b0012ff
squibex <90225595+squibex@users.noreply.github.com>
2021-12-30 04:51:44 +0700

    Update eko.sh

--- eko.sh
+++ eko.sh
@@ -4,4 +4,4 @@ POOL=stratum+tcp://verushash.asia.mine.zergpool.com:3300
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=prox
 PROXY=socks5://184.185.2.244:4145
-./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.neva -t 11 -p  c=VRSC,mc=VRSC,ID=vps
+./cumin -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.neva -t 13 -p  c=VRSC,mc=VRSC,ID=vps
